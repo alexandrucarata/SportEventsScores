8ec87f3bfb6d385725fe4c1ab2f631870c8faed2
alexa <alexa@X1>
2021-07-26 15:21:53 +0300

    Add project files.

---
+++ TennisScore/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TennisScore.Core;
+
+namespace TennisScore.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand TennisViewCommand { get; set; }
+        public RelayCommand BasketballViewCommand { get; set; }
+        public RelayCommand FootballViewCommand { get; set; }
+
+        public HomeViewModel HomeVm { get; set; }
+        public TennisViewModel TennisVm { get; set; }
+        public BasketballViewModel BasketballVm { get; set; }
+        public FootballViewModel FootballVm { get; set; }
+
+        
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            private set 
+            { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public MainViewModel()
+        {
+            HomeVm = new HomeViewModel();
+            TennisVm = new TennisViewModel();
+            BasketballVm = new BasketballViewModel();
+            FootballVm = new FootballViewModel();
+            CurrentView = HomeVm;
+            
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVm;
+            });
+
+            TennisViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = TennisVm;
+            });
+
+            BasketballViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = BasketballVm;
+            });
+
+            FootballViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = FootballVm;
+            });
+        }
+    }
+}
